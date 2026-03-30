798f8c40cb4608404046d95a7c9b096a5c9913ed
Ubuntu995 <za00011@163.com>
2026-03-30 20:27:36 +0800

    update

--- EventLoopThreadPool.h
+++ EventLoopThreadPool.h
@@ -12,7 +12,7 @@ class EventLoopThread;
 class EventLoopThreadPool : noncopyable
 {
 public:
-    using ThreadInitCallback = std::function<void(EventLoop*)>; 
+    using ThreadInitCallback = std::function<void(EventLoop *)>;
 
     EventLoopThreadPool(EventLoop *baseLoop, const std::string &nameArg);
     ~EventLoopThreadPool();
@@ -22,19 +22,19 @@ public:
     void start(const ThreadInitCallback &cb = ThreadInitCallback());
 
     // 如果工作在多线程中，baseLoop_默认以轮询的方式分配channel给subloop
-    EventLoop* getNextLoop();
+    EventLoop *getNextLoop();
 
-    std::vector<EventLoop*> getAllLoops();
+    std::vector<EventLoop *> getAllLoops();
 
     bool started() const { return started_; }
     const std::string name() const { return name_; }
-private:
 
-    EventLoop *baseLoop_; // EventLoop loop;  
+private:
+    EventLoop *baseLoop_; // EventLoop loop;   主线程的EventLoop（MainReactor）
     std::string name_;
     bool started_;
     int numThreads_;
     int next_;
     std::vector<std::unique_ptr<EventLoopThread>> threads_;
-    std::vector<EventLoop*> loops_;
+    std::vector<EventLoop *> loops_;
 };
