a6809da35e86f7fa5c7ea5a15b213c41484558aa
Ubuntu995 <za00011@163.com>
2026-04-02 15:03:28 +0800

    update

--- TcpConnection.h
+++ TcpConnection.h
@@ -17,22 +17,26 @@ class Socket;
 /**
  * TcpServer => Acceptor => 有一个新用户连接，通过accept函数拿到connfd
  * =》 TcpConnection 设置回调 =》 Channel =》 Poller =》 Channel的回调操作
- * 
- */ 
+ *
+ *
+ * 封装一个已建立的 TCP 连接的所有细节，负责连接的建立、数据收发、断开、生命周期管理，
+ * 是服务器与单个客户端通信的唯一桥梁，也是业务层和网络底层的交互入口。
+ *
+ */
 class TcpConnection : noncopyable, public std::enable_shared_from_this<TcpConnection>
 {
 public:
-    TcpConnection(EventLoop *loop, 
-                const std::string &name, 
-                int sockfd,
-                const InetAddress& localAddr,
-                const InetAddress& peerAddr);
+    TcpConnection(EventLoop *loop,
+                  const std::string &name,
+                  int sockfd,
+                  const InetAddress &localAddr,
+                  const InetAddress &peerAddr);
     ~TcpConnection();
 
-    EventLoop* getLoop() const { return loop_; }
-    const std::string& name() const { return name_; }
-    const InetAddress& localAddress() const { return localAddr_; }
-    const InetAddress& peerAddress() const { return peerAddr_; }
+    EventLoop *getLoop() const { return loop_; }
+    const std::string &name() const { return name_; }
+    const InetAddress &localAddress() const { return localAddr_; }
+    const InetAddress &peerAddress() const { return peerAddr_; }
 
     bool connected() const { return state_ == kConnected; }
 
@@ -41,27 +45,45 @@ public:
     // 关闭连接
     void shutdown();
 
-    void setConnectionCallback(const ConnectionCallback& cb)
-    { connectionCallback_ = cb; }
+    void setConnectionCallback(const ConnectionCallback &cb)
+    {
+        connectionCallback_ = cb;
+    }
 
-    void setMessageCallback(const MessageCallback& cb)
-    { messageCallback_ = cb; }
+    void setMessageCallback(const MessageCallback &cb)
+    {
+        messageCallback_ = cb;
+    }
 
-    void setWriteCompleteCallback(const WriteCompleteCallback& cb)
-    { writeCompleteCallback_ = cb; }
+    void setWriteCompleteCallback(const WriteCompleteCallback &cb)
+    {
+        writeCompleteCallback_ = cb;
+    }
 
-    void setHighWaterMarkCallback(const HighWaterMarkCallback& cb, size_t highWaterMark)
-    { highWaterMarkCallback_ = cb; highWaterMark_ = highWaterMark; }
+    void setHighWaterMarkCallback(const HighWaterMarkCallback &cb, size_t highWaterMark)
+    {
+        highWaterMarkCallback_ = cb;
+        highWaterMark_ = highWaterMark;
+    }
 
-    void setCloseCallback(const CloseCallback& cb)
-    { closeCallback_ = cb; }
+    void setCloseCallback(const CloseCallback &cb)
+    {
+        closeCallback_ = cb;
+    }
 
     // 连接建立
     void connectEstablished();
     // 连接销毁
     void connectDestroyed();
+
 private:
-    enum StateE {kDisconnected, kConnecting, kConnected, kDisconnecting};
+    enum StateE
+    {
+        kDisconnected,
+        kConnecting,
+        kConnected,
+        kDisconnecting
+    };
     void setState(StateE state) { state_ = state; }
 
     void handleRead(Timestamp receiveTime);
@@ -69,7 +91,7 @@ private:
     void handleClose();
     void handleError();
 
-    void sendInLoop(const void* message, size_t len);
+    void sendInLoop(const void *message, size_t len);
     void shutdownInLoop();
 
     EventLoop *loop_; // 这里绝对不是baseLoop， 因为TcpConnection都是在subLoop里面管理的
@@ -84,8 +106,8 @@ private:
     const InetAddress localAddr_;
     const InetAddress peerAddr_;
 
-    ConnectionCallback connectionCallback_; // 有新连接时的回调
-    MessageCallback messageCallback_; // 有读写消息时的回调
+    ConnectionCallback connectionCallback_;       // 有新连接时的回调
+    MessageCallback messageCallback_;             // 有读写消息时的回调
     WriteCompleteCallback writeCompleteCallback_; // 消息发送完成以后的回调
     HighWaterMarkCallback highWaterMarkCallback_;
     CloseCallback closeCallback_;
